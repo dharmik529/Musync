86d0d0cb9c754fcb2cef634f9487102c1678e67e
dharmikpatel <dharmik529@gmail.com>
2023-10-12 13:21:10 -0400

    serialized

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use serde::{Serialize, Deserialize};
 use dotenv::dotenv;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct SpotifyKeys {
+    spotify_client_id : String,
+    spotify_client_secret : String,
+    spotify_redirect_url : String,
+    spotify_access_token : Option<String>,
+}
+
 fn main() {
     dotenv().ok();
-    let client_id = std::env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID must be set.");
-    println!("SPOTIFY_CLIENT_ID = {}", client_id);
+    let spotify_keys = get_access_tocken();
+    let serialized = serde_json::to_string(&spotify_keys).unwrap();
+    println!("serialized = {}", serialized);
+}
+
+fn get_access_tocken() ->  SpotifyKeys {
+    
+    let spotify_keys = SpotifyKeys {
+        spotify_client_id : std::env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID must be set."),
+        spotify_client_secret : std::env::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET must be set."),
+        spotify_redirect_url : std::env::var("SPOTIFY_REDIRECT_URI").expect("SPOTIFY_REDIRECT_URI must be set."),
+        spotify_access_token : None
+    };
+
+    return spotify_keys;
 }
