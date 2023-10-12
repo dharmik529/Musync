79577759de0d455a038d6805bb856066bf92821e
dharmikpatel <dharmik529@gmail.com>
2023-10-12 12:46:00 -0400

    succesfully read from .env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use serde::{Serialize, Deserialize};
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let client_id = std::env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID must be set.");
+    println!("SPOTIFY_CLIENT_ID = {}", client_id);
 }
